8a05cd6b2cf109e85ccb9cc5f986ef59168cb2db
Shlomo_Desktop_new <mail@madrichim.ovh>
2022-01-22 21:05:02 +0200

    הוספת הדפסת המידע שמתקבל

--- Hooks-NodeBB-Tools.user.js
+++ Hooks-NodeBB-Tools.user.js
@@ -1,41 +1,44 @@
 // ==UserScript==
 // @name         Hooks NodeBB Tools
 // @namespace    https://madrichim.ovh
-// @version      0.1
+// @version      0.2
 // @updateURL    https://github.com/ShlomoCode/Hooks-NodeBB-Tools/raw/main/
-// @description  כלי בדיקה לhooks עבור אתרי NodeBB
+// @description  כלי בדיקת הוקים לאתרי נודביבי
 // @author       ShlomoCode
 // @match        https://mitmachim.top/*
 // @match        https://tchumim.com/*
 // ==/UserScript==
-
 function testAction() {
     let action;
     action = prompt("אנא הכנס שם הוק מלא שברצונך לבדוק.");
     if (action) {
-      app.alert({
-        title: "הצלחה!",
-        message: `ההוק "${action}" התווסף למצב בדיקה.`,
-        location: "left-top",
-        timeout: 2200,
-        type: "success",
-      });
-      $(window).on(action, () => {
-        alert(`שים לב: ההוק "${action}" הופעל כעת!`);
-      });
+        app.alert({
+            title: "הצלחה!",
+            message: `ההוק "${action}" התווסף למצב בדיקה.`,
+            location: "left-top",
+            timeout: 2200,
+            type: "success",
+        });
+        $(window).on(action, function(event, data) {
+            console.log("Data:");
+            console.log(data);
+            console.log("Event:");
+            console.log(event);
+            alert(`שים לב: ההוק "${action}" הופעל כעת!`);
+        });
     } else {
-      app.alert({
-        title: "שגיאה!",
-        message: "לא הוזן שם הוק.",
-        location: "left-top",
-        timeout: 2200,
-        type: "info",
-      });
+        app.alert({
+            title: "שגיאה!",
+            message: "לא הוזן שם הוק.",
+            location: "left-top",
+            timeout: 2200,
+            type: "info",
+        });
     }
-  }
-  
-  document.addEventListener("keydown", function (event) {
+}
+
+document.addEventListener("keydown", function(event) {
     if (event.ctrlKey && event.which === 81) {
-      testAction();
+        testAction();
     }
-  });
+});
